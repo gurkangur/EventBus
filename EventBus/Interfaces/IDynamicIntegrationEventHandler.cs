79ba7121988935888ff316ef8cdb6c634dcebfb3
Gurkan <gurkan.gur@ozdilek.com.tr>
2021-01-31 20:07:15 +0300

    initial

---
+++ EventBus/Interfaces/IDynamicIntegrationEventHandler.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace EventBus.Interfaces
+{
+    public interface IDynamicIntegrationEventHandler
+    {
+        Task Handle(dynamic eventData);
+    }
+}
---
+++ EventBus/Interfaces/IIntegrationEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using EventBus.Events;
+using System.Threading.Tasks;
+
+namespace EventBus.Interfaces
+{
+    public interface IIntegrationEventHandler<in TIntegrationEvent> : IIntegrationEventHandler
+            where TIntegrationEvent : IntegrationEvent
+    {
+        Task Handle(TIntegrationEvent @event);
+    }
+
+    public interface IIntegrationEventHandler
+    {
+    }
+}
